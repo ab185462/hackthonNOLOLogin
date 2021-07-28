7a37595ae82ff2d9c61b3aaed076854dd935aff7
Ambica <ab185462@ncr.com>
2021-07-29 01:20:06 +0530

    NOLOLogin

---
+++ build/precache-manifest.17b68d1b606abed4381407372edc915c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a703d82707a83187b7e6bfa3b16086d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e041bdcef112b09bcd40",
+    "url": "/static/css/main.184e7dff.chunk.css"
+  },
+  {
+    "revision": "3e2508b349c57775725b",
+    "url": "/static/js/2.bf527581.chunk.js"
+  },
+  {
+    "revision": "2d21d72fd4a5b94d8a3d931708b3a215",
+    "url": "/static/js/2.bf527581.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e041bdcef112b09bcd40",
+    "url": "/static/js/main.776141b6.chunk.js"
+  },
+  {
+    "revision": "0a2196dc6f656b36fa82",
+    "url": "/static/js/runtime-main.007f975e.js"
+  }
+]);
